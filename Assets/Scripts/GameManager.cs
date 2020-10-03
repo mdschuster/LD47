c2307cdceb43bdc0de8dfdbb142d8b921f137bf5
Micah Schuster <micahdschuster@gmail.com>
2020-10-02 21:30:27 -0400

    Create Singleton GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    //Singleton section
+    private static GameManager _instance = null;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+    }
+
+    public static GameManager instance()
+    {
+        return _instance;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
