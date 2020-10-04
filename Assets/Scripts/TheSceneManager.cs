4b0000053dd922d50e6ec0e71601eaf9b6204134
Micah Schuster <micahdschuster@gmail.com>
2020-10-04 09:40:18 -0400

    Create scene manager, link buttons to load game and quit

---
+++ Assets/Scripts/TheSceneManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TheSceneManager : MonoBehaviour
+{
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void loadMainGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void loadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void quit()
+    {
+        Application.Quit(0);
+    }
+}
