8ee0e9fcece5642ab9b163c6c65b758b56c64f99
Micah Schuster <micahdschuster@gmail.com>
2020-10-04 14:54:38 -0400

    Add main music loop and manager

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicManager : MonoBehaviour
+{
+
+    public AudioClip BackgroundMusic;
+
+    static MusicManager _instance = null;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource source = GetComponent<AudioSource>();
+        source.clip = BackgroundMusic;
+
+        //play background music here
+        if (!source.isPlaying)
+        {
+            source.Play();
+        }
+    }
+}
